169c1d030c7d914099c3d65c75dbf4de345d0591
jasonshere <mrjasonedu@gmail.com>
2021-09-26 16:57:27 +1000

    update

--- start_experiments.py
+++ start_experiments.py
@@ -3,6 +3,7 @@ import argparse
 
 parser = argparse.ArgumentParser(description="Run sample main.")
 parser.add_argument('--config', type=str, default='sample_hello_world')
+parser.add_argument('--fold', type=str, default='1')
 args = parser.parse_args()
 
-run_experiment(f"config_files/{args.config}.yml")
+run_experiment(f"config_files/{args.config}.yml", args.fold)
